722353320c8cbd7ffb31abd6befd1b8d7dc69036
Dwij13 <dwijjindal2005@gmail.com>
2024-01-25 10:03:32 +0530

    Added server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/ping',(req,res)=>{
+    res.send("pong")
+})
+
+app.get('/',(req,res)=>{
+    res.send("root path")
+})
+
+app.listen(port,()=>{
+    console.log(`App is listening on ${port}`)
+})
