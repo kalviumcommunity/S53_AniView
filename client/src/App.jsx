48d3ec2943ff5646e2e0a045196adf5e745e7778
Dwij13 <dwijjindal2005@gmail.com>
2024-02-05 13:49:26 +0530

    Created a landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+  )
+}
