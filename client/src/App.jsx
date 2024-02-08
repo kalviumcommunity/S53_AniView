32b1e9ccfeb10e438869e6f98b5b62c09950ef6f
Dwij13 <dwijjindal2005@gmail.com>
2024-02-08 11:19:08 +0530

    correcting of css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import Home from './components/Home'
-import Listing from './components/Listing'
-import Navbar from './components/Navbar'
-import {Routes,Route} from "react-router-dom"
-
+import Home from "./components/Home";
+import Listing from "./components/Listing";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
+    <div className="bg-img"></div>
+      <Navbar />
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/list' element={<Listing/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<Listing />} />
       </Routes>
     </div>
-  )
+  );
 }
